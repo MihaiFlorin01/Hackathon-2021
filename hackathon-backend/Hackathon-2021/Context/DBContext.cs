c88e54c434d363071a1c591e604b1590a0938afd
Ene Remus <remusene69@gmail.com>
2021-11-13 10:12:42 +0200

    Start project
    Added helper NuGets for DB
    Added API controller for Courses
    Added DBContext
    Added CourseClass and UserClass

---
+++ hackathon-backend/Hackathon-2021/Context/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using Hackathon_2021.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon_2021.Context
+{
+    public class DBContext:DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> Options):base(Options)
+        {
+        }
+
+        public DbSet<User> userDBSet { get; set; }
+        public DbSet<Courses> courseDBSet { get; set; }
+    }
+}
---
+++ hackathon-backend/Hackathon-2021/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon_2021.Model
+{
+    public class User
+    {
+        public int _UserID { get; set; }
+        public string _Username { get; set; }
+        public string _Password { get; set; }
+        public string _Email { get; set; }
+        public Courses courses { get; set; }
+
+
+
+    }
+}
